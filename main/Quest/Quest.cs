6cc2996af08a03341eb4c2e12b1e8aeb4c7d4ae7
AmineArr <Aminearram2007@gmail.com>
2025-09-10 15:58:16 +0200

    quest class further updated

--- main/Quest/Quest.cs
+++ main/Quest/Quest.cs
@@ -1,11 +1,23 @@
 ﻿public class Quest
 {
+    public int ID; // Unieke quest ID
     public string QuestTitle;
     public string Description;
+    public int RewardGold;
+    public bool IsCompleted;
 
-    public Quest(string questTitle, string description)
+    public Quest(int id, string questTitle, string description, int rewardGold = 0)
     {
+        ID = id;
         QuestTitle = questTitle;
         Description = description;
+        RewardGold = rewardGold;
+        IsCompleted = false;
+    }
+
+    public void Complete()
+    {
+        IsCompleted = true;
+        
     }
 }
