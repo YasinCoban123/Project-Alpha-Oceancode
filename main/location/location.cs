c1d31a24265f4bdcedf0eb64b5a1f2882cf2d891
YasinCoban123 <9018681@student.zadkine.nl>
2025-09-10 14:47:21 +0200

    test push to seee compatibility

--- main/location/location.cs
+++ main/location/location.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Location
+{
+    public int ID;
+    public string Name;
+    public string Description;
+
+    public Location LocationToNorth;
+    public Location LocationToSouth;
+    public Location LocationToEast;
+    public Location LocationToWest;
+
+    public Location(int id, string name, string description)
+    {
+        ID = id;
+        Name = name;
+        Description = description;
+    }
+
+    public Location Move(string direction)
+    {
+        direction = direction.ToLower();
+
+        if (direction == "n") return LocationToNorth;
+        if (direction == "s") return LocationToSouth;
+        if (direction == "e") return LocationToEast;
+        if (direction == "w") return LocationToWest;
+
+        return null;
+    }
+}
