d1afd4e853ca1b4e3b5c573ad2c5d422676ce6bc
Arjun-adl <1098271@hr.nl>
2025-09-15 05:15:20 +0200

    inventory systeme and reward system push

---
+++ main/monster/Rewards/Rewards.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class RewardsSystem
+{
+    public Inventory InventorySys;
+
+    public RewardsSystem(Inventory inventory)
+    {
+        InventorySys = inventory;
+    }
+
+
+    public void GrantReward(string RewardType)
+    {
+        switch (RewardType.ToLower())
+        {
+            case "Frog eg":
+                _inventory.AddItem(new Item("Frog leg", 1));
+                Console.WriteLine("You received a Frog leg!");
+                break;
+
+            case "Rusty Armor":
+                _inventory.AddItem(new Item("Rusty Armor", 1));
+                Console.WriteLine("You received a Rusty Armor!");
+                break;
+
+            case "Sturdy Sword":
+                _inventory.AddItem(new Weapon(World.WEAPON_ID_RUSTY_SWORD, "Sturdy Sword", 5));
+                Console.WriteLine("You received a Sturdy Sword!");
+                break;
+
+            case "Mace":
+                _inventory.AddItem(new Weapon(World.WEAPON_ID_CLUB, "Mace", 10));
+                Console.WriteLine("You received a Mace!");
+                break;
+
+            default:
+                Console.WriteLine("No reward found for this type.");
+                break;
+        }
+    }
+
+    // Optionally: Randomized reward system
+    public void GrantRandomReward()
+    {
+        Random rand = new Random();
+        int roll = rand.Next(1, 4);
+
+        if (roll == 1)
+            GrantReward("Frog leg");
+        else if (roll == 2)
+            GrantReward("Rusty Armor");
+        else if (roll == 3)
+            GrantReward("Sturdy Sword");
+        else
+            GrantReward("Mace");
+    }
+}
---
+++ main/player/InventoryManagment/InventoryManagment.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Item
+{
+    public string Name;
+    public int Amount;
+
+    public Item(string name, int amount = 1)
+    {
+        Name = name;
+        Amount = amount;
+    }
+
+    public override string ToString()
+    {
+        return $"Item: {Name}, Amount: {Amount}";
+    }
+}
+
+// Weapon class inherits from Item
+public class Weapon : Item
+{
+    public int Damage;
+    public int ID;
+
+    public Weapon(int id, string name, int damage, int amount = 1) 
+        : base(name, amount)
+    {
+        ID = id;
+        Damage = damage;
+    }
+
+    public override string ToString()
+    {
+        return $"Weapon: {Name}, Damage: {Damage}, Amount: {Amount}";
+    }
+}
+
+// Inventory class
+public class Inventory
+{
+    private List<Item> items = new List<Item>();
+
+    // Preload some items and weapons for demo
+    public Inventory()
+    {
+        // Add generic items
+        items.Add(new Item("Frog legs", 3));
+        items.Add(new Item("Torch", 2));
+
+        // Add predefined weapons from World
+        items.Add(World.WeaponByID(World.WEAPON_ID_RUSTY_SWORD));
+        items.Add(World.WeaponByID(World.WEAPON_ID_CLUB));
+    }
+
+    // Show only items (non-weapons)
+    public void ShowItems()
+    {
+        List<Item> nonWeapons = items.Where(i => !(i is Weapon)).ToList();
+        if (nonWeapons.Count == 0)
+        {
+            Console.WriteLine("No items in inventory.");
+        }
+        else
+        {
+            Console.WriteLine("Items in Inventory:");
+            foreach (Item item in nonWeapons)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    // Show only weapons
+    public void ShowWeapons()
+    {
+        List<Weapon> weapons = items.OfType<Weapon>().ToList();
+        if (weapons.Count == 0)
+        {
+            Console.WriteLine("No weapons in inventory.");
+        }
+        else
+        {
+            Console.WriteLine("Weapons in Inventory:");
+            foreach (Weapon weapon in weapons)
+            {
+                Console.WriteLine(weapon);
+            }
+        }
+    }
+}
+
+// World class for predefined content
+public static class World
+{
+    public static readonly List<Weapon> Weapons = new List<Weapon>();
+
+    public const int WEAPON_ID_RUSTY_SWORD = 1;
+    public const int WEAPON_ID_CLUB = 2;
+
+    static World()
+    {
+        PopulateWeapons();
+    }
+
+    public static void PopulateWeapons()
+    {
+        Weapons.Add(new Weapon(WEAPON_ID_RUSTY_SWORD, "Rusty Sword", 5));
+        Weapons.Add(new Weapon(WEAPON_ID_CLUB, "Club", 10));
+    }
+
+    public static Weapon WeaponByID(int id)
+    {
+        return Weapons.FirstOrDefault(w => w.ID == id);
+    }
+}
+
+// Main program
+class Program
+{
+    static void Main(string[] args)
+    {
+        Inventory inventory = new Inventory();
+        bool running = true;
+
+        Console.WriteLine("Welcome to the Inventory System!");
+
+        while (running)
+        {
+            Console.WriteLine("\nCommands: show items, show weapons, exit");
+            Console.Write("Enter command: ");
+            string command = Console.ReadLine().ToLower();
+
+            switch (command)
+            {
+                case "show items":
+                    inventory.ShowItems();
+                    break;
+
+                case "show weapons":
+                    inventory.ShowWeapons();
+                    break;
+
+                case "exit":
+                    running = false;
+                    Console.WriteLine("Getting back to the game");
+                    break;
+
+                default:
+                    Console.WriteLine("Unknown command!");
+                    break;
+            }
+        }
+    }
+}
