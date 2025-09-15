fa0c738a7b94843d7dc9d812122985baf2f01222
AmineArr <Aminearram2007@gmail.com>
2025-09-15 13:22:55 +0200

    Cleaned up and minimized the Quest class

--- main/Quest/Quest.cs
+++ main/Quest/Quest.cs
@@ -1,23 +1,26 @@
 ﻿public class Quest
 {
-    public int ID; // Unieke quest ID
     public string QuestTitle;
     public string Description;
-    public int RewardGold;
     public bool IsCompleted;
 
-    public Quest(int id, string questTitle, string description, int rewardGold = 0)
+    public Quest(string questTitle, string description)
     {
-        ID = id;
         QuestTitle = questTitle;
         Description = description;
-        RewardGold = rewardGold;
         IsCompleted = false;
     }
+    
+    public void QuestStarted()
+    {
+        Console.WriteLine($"Quest gestart: {QuestTitle}");
+        Console.WriteLine(Description);
+    }
 
-    public void Complete()
+    public void QuestCompleted()
     {
         IsCompleted = true;
-        
+        Console.WriteLine($"Quest voltooid: {QuestTitle}");
+
     }
 }
